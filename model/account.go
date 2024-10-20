5f186d202870aecb2c089cad6459e1644ec584d3
KimGyeongsuuu <sk62628504@gmail.com>
2024-10-20 22:31:44 +0900

    add :: enum

--- model/account.go
+++ model/account.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"context"
 	"time"
 )
 
@@ -9,9 +10,36 @@ type Account struct {
 	Password   string    `gorm:"not null"`
 	Grade      int       `gorm:"not null"`
 	Name       string    `gorm:"type:varchar(10);not null"`
-	Gender     string    `gorm:"type:varchar(10);not null"`
-	Major      string    `gorm:"type:varchar(20);not null"`
+	Gender     Gender    `gorm:"type:varchar(10);not null"`
+	Major      Major     `gorm:"type:varchar(20);not null"`
 	ProfileURL *string   `gorm:"type:text"`
-	Authority  string    `gorm:"type:varchar(20);not null"`
+	Authority  Authority `gorm:"type:varchar(20);not null"`
 	CreatedAt  time.Time `gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP"`
 }
+
+type AccountRepository interface {
+	CreateAccount(ctx context.Context, account *Account) error
+	ExistsByEmail(ctx context.Context, email string) (bool, error)
+}
+
+type Authority string
+
+const (
+	ROLE_STUDENT         Authority = "ROLE_STUDENT"
+	ROLE_STUDENT_COUNCIL Authority = "ROLE_STUDENT_COUNCIL"
+)
+
+type Gender string
+
+const (
+	MAN   Gender = "MAN"
+	WOMAN Gender = "WOMAN"
+)
+
+type Major string
+
+const (
+	SW_DEVELOP Major = "SW_DEVELOP"
+	SMART_IOT  Major = "SMART_IOT"
+	AI         Major = "AI"
+)
