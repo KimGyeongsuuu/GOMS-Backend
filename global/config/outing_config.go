90cfe03e1e206c0dce610d63081f36486e11ece4
KimGyeongsuuu <sk62628504@gmail.com>
2024-11-06 11:10:35 +0900

    update :: app.yml

--- global/config/outing_config.go
+++ global/config/outing_config.go
@@ -1,36 +1,31 @@
 package config
 
-import (
-	"os"
-	"strconv"
-)
+// type OutingProperties struct {
+// 	OutingExp int64
+// }
 
-type OutingProperties struct {
-	OutingExp int64
-}
+// type OutingBlackListProperties struct {
+// 	OutingBlackListExp int64
+// }
 
-type OutingBlackListProperties struct {
-	OutingBlackListExp int64
-}
+// func LoadOutingProperties() (*OutingProperties, error) {
+// 	outingExp, err := strconv.ParseInt(os.Getenv("OUTING_EXP"), 10, 64)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-func LoadOutingProperties() (*OutingProperties, error) {
-	outingExp, err := strconv.ParseInt(os.Getenv("OUTING_EXP"), 10, 64)
-	if err != nil {
-		return nil, err
-	}
+// 	return &OutingProperties{
+// 		OutingExp: outingExp,
+// 	}, nil
+// }
 
-	return &OutingProperties{
-		OutingExp: outingExp,
-	}, nil
-}
+// func LoadOutingBlackListProperties() (*OutingBlackListProperties, error) {
+// 	outingBlackListExp, err := strconv.ParseInt(os.Getenv("OUTING_BLACK_LIST_EXP"), 10, 64)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-func LoadOutingBlackListProperties() (*OutingBlackListProperties, error) {
-	outingBlackListExp, err := strconv.ParseInt(os.Getenv("OUTING_BLACK_LIST_EXP"), 10, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	return &OutingBlackListProperties{
-		OutingBlackListExp: outingBlackListExp,
-	}, nil
-}
+// 	return &OutingBlackListProperties{
+// 		OutingBlackListExp: outingBlackListExp,
+// 	}, nil
+// }
